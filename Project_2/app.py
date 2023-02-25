81e9acf110e84c9b7b99d55c890d04aa56a542bd
jhashivam2200 <76090863+jhashivam0022@users.noreply.github.com>
2023-02-25 17:25:09 +0530

    commit

---
+++ Project_2/app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.title('Hello! I am :blue[Shivam kumar jha] from Hyderabad, India.')
+st.subheader('Data Science Internship at Innomatics Research Lab.')
+st.subheader('Thank You :red[Innomatics] for giving me this opportunity.')
